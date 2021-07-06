ecee9c8ee0681ef3592075f5505251f6631fef80
Uday Sai Tyada <udaysaityada@Udays-MacBook-Pro.local>
2021-07-06 10:08:57 +0530

    pointing to american hood

--- scripts/mapInitializer.js
+++ scripts/mapInitializer.js
@@ -3,8 +3,9 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 5,
-    center: { lat: -28, lng: 137 },
+    center: { lat:    40.617915, lng: -97.076322 },
   });
   // NOTE: This uses cross-domain XHR, and may not work on older browsers.
-  map.data.loadGeoJson("/geoJsonSources/world-geojson/countriesStateWise/australia.geojson");
+  // map.data.loadGeoJson("/geoJsonSources/world-geojson/countriesStateWise/australia.geojson");
+  map.data.loadGeoJson("/geoJsonSources/hoods.geojson");
 }
